7f40bccc091f369f70a9fec8a34c8b97feb76738
wsqseventeen <1393503592@qq.com>
2019-11-12 16:00:21 +0800

    added  ex.c file

---
+++ lab2/ex.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    unsigned int a=1;
+    unsigned int b=0;
+    unsigned int c=0-1;
+    while (a!=0){
+        a=a<<1;
+        b=b+1
+    }
+    printf("a=%u\n",c);
+    printf("%d",b);
+}
