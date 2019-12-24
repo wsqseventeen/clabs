a3b9e7ff03c1cd472f88c23df004124f4b28ee7c
wsqseventeen <1393503592@qq.com>
2019-12-24 15:32:58 +0800

    add to ex2

--- lab3/ex1.c
+++ lab3/ex1.c
@@ -1,71 +1,32 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#define MAXLINE 1000
-int get_line(char *s, int lim);
-int strindex(char *s, char *t);
-char pattern[MAXLINE];
+#include<stdio.h>
+#define yes 1
+#define no 0
 int main()
 {
- char line[MAXLINE];
- int found;
- while (1)
- {
-  get_line(line, MAXLINE);
-  get_line(pattern, MAXLINE);
-  found = strindex(line, pattern);
-  if (found != -1)
-  {
-   printf("%d\n",found) ;
-  }
-  else
-   printf("-1 does not contain t!\n");
- }
- return 0;
+return 0;
 }
-/*
-int main()
+int htoi(char s[])
 {
- char s[100] = "fsafsafafafafaf";
- char t[100] = "s";
- int i;
- i = strindex(s,t);
- printf("i=%d\n", i);
+int hexdigit ,n, inhex,i;
+i=0;
+if(s[i]=='0')
+{
+i++;
+if(s[i]=='x'||s[i]=='X')
+{
+i++;
 }
-*/
-
-
-
-
-int get_line(char *s, int lim)
-{
- int c, i;
- i=0;
- while (--lim>0&&(c=getchar())!=EOF&&c!='\n')
-  s[i++]=c;
- if (c=='\n')
-  s[i++]='\0';
- return i;
 }
-int strindex(char *s, char *t)
+n=0;
+inhex=yes;
+for(;inhex==yes;i++)
 {
- int i, j, k, cc=-1, jud=0;
- for (i=0; s[i]!='\0'; i++)
- {
-  for (j=i, k=0; t[k]!='\0'&&s[j]==t[k]; j++, k++)
-   {
-    jud++;
-   }
- 
-  if (k>0&&t[k]=='\0')
- 
-   if(i>cc)
-   {
-    cc=i;
-   }
- }
- if (jud!=0)
-  return cc;
- else
-  return -1;
+if(s[i]>='0'&&s[i]<='9') hexdigit=s[i]-'0';
+else if(s[i]>='a'&&s[i]<='f')hexdigit=s[i]-'a'+10;
+else if(s[i]>='A'&&s[i]<='F')hexdigit=s[i]-'A'+10;
+else inhex=no;
+if(inhex==yes)
+n=16*n+hexdigit;
+}
+return n;
 }
